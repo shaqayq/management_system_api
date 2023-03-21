a7393da4f06ddc72ba1dc15614f1c7244f89a3fd
shaqayq <shaqayq.darwazi@gmail.com>
2023-03-21 12:25:15 +0430

    install body-parser and cors

--- app/index.js
+++ app/index.js
@@ -1,1 +1,10 @@
 //File for arrange a programm components like Routs,Middelwar,...
+
+const express = require('express');
+const app = express()
+
+module.exports = (port) => {
+    app.listen(port,()=>{
+        console.log(`app is running on port: ${port} `);
+    })
+}
