0b58bbf31809b33adc22a63664f1430770b381b4
shaqayq <shaqayq.darwazi@gmail.com>
2023-03-22 22:06:52 +0430

    creat mysql connection

---
+++ app/boot/mysql.js
@@ -0,0 +1,20 @@
+const mysql= require('mysql')
+
+const {MYSQL_HOST , MYSQL_DB} = process.env
+
+const connection = mysql.createConnection({
+    host: MYSQL_HOST, 
+    database: MYSQL_DB , 
+    user: 'root'
+});
+
+const startMysql = () =>{
+connection.connect(function(err){
+    if(err){
+        console.log('Error connecting');
+    }
+
+    console.log('Connect Successfully');
+})}
+
+module.exports = startMysql
